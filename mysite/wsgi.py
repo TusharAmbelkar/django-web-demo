13327d21ca4bc3cab9448f7008b21a40ccdab428
TusharAmbelkar <tusharambelkar@gmail.com>
2019-04-07 17:57:06 +0530

    Changed improper Procfile

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -18,6 +18,6 @@ from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+#application = DjangoWhiteNoise(application)
 
 
